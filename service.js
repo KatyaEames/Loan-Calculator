f4553a2309e37e620e04f437201d421e46c501f2
KatyaEames <katya.eames@gmail.com>
2015-02-09 12:11:11 -0700

    math.random

---
+++ service.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+var port = 8080;
+
+app.use(express.static(_dirname + '/ public'));
+
+app.get('/interest_rate', function(req, res) { //GET endpoint
+	var randomNumber = Math.floor(Math.random() * 2000) / 100
+	res.json(randomNumber);
+})
+
+app.listen(port, function() {
+	console.log('Now listening on port ' + port);
+})
